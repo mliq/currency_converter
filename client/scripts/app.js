2c69c9eb214f8636e908631af26cbe32696f97f1
mliq <liquori@gmail.com>
2015-05-18 17:01:38 -0500

    Custom currencies work and symbols too

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,28 @@
 var myApp = angular.module('myApp', []);
 var from = "USD", to = "EUR";
 
-myApp.controller('CurrencyController', ['$scope', '$http',
-    function ($scope, $http) {
+myApp.controller('CurrencyController', ['$scope', '$http', '$filter',
+    function ($scope, $http, $filter) {
         $scope.heading = "Enter Amount: ";
+        var symbol;
 
         $scope.convert = function (curr) {
-            from = curr.slice(0,3);
-            to = curr.slice(3);
 
-            $http.get('/currency/get?from='+from+'&to='+to).
+            if (curr == "special") {
+                from = $scope.from;
+                to = $scope.to;
+            } else {
+                from = curr.slice(0, 3);
+                to = curr.slice(3);
+            }
+
+            var symbol = to == "USD" ? symbol = "$" : symbol = to+"$";
+            if (to=="EUR"){symbol="€"}
+
+            $http.get('/currency/get?from=' + from + '&to=' + to).
                 success(function (data) {
                     console.log(data.rate);
-                    //$scope.message = $scope.amt * data.rate;
+                    $scope.message = $filter('currency')($scope.amt * data.rate, symbol);
                 });
         };
 
