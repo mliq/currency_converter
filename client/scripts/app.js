9c33bd230a174eab1cf97a7ced82160c77b20f99
mliq <liquori@gmail.com>
2015-05-18 16:10:18 -0500

    Going to try and get CORS working by using node to fetch data

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,22 @@
 var myApp = angular.module('myApp', []);
+var from = "USD", to = "EUR";
 
-myApp.controller('WelcomeController',["$scope", function($scope){
-    $scope.heading = "Message: ";
+myApp.controller('CurrencyController', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.heading = "Enter Amount: ";
 
-    $scope.updateMessage = function(){
-        $scope.message = "Hello " + $scope.name + "!";
-    };
+        $scope.convert = function (curr) {
+            if (curr == "EURUSD") {
+                from = "EUR";
+                to = "USD";
+            }
+            $http.get('http://jsonrates.com/get/?from=' + from + '&to=' + to + '&apiKey=jr-5be588f08ecd7855e0c8266a4aeb7d36').
+                success(function (data) {
+                    console.log(data);
+                    $scope.message = $scope.amt * data.rate;
+                });
+        };
 
-    $scope.goodbyeMessage = function(){
-        $scope.message = "Goodbye, " + $scope.name + " :("
     }
-}]);
+])
+;
