135702051d563cd7dc99540d9642a02b1603facc
mliq <liquori@gmail.com>
2015-05-18 21:57:54 -0500

    auto uppercase input

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,26 +3,33 @@ var from = "USD", to = "EUR";
 
 myApp.controller('CurrencyController', ['$scope', '$http', '$filter',
     function ($scope, $http, $filter) {
-        $scope.heading = "Enter Amount: ";
+        $scope.message = "Enter Amount: ";
         var symbol;
 
         $scope.convert = function (curr) {
 
             if (curr == "special") {
-                from = $scope.from;
-                to = $scope.to;
+                from = $scope.from.toUpperCase();
+                to = $scope.to.toUpperCase();
             } else {
                 from = curr.slice(0, 3);
                 to = curr.slice(3);
             }
 
-            var symbol = to == "USD" ? symbol = "$" : symbol = to+"$";
-            if (to=="EUR"){symbol="€"}
+            var symbol = to == "USD" ? symbol = "$" : symbol = to + " ";
+            if (to == "EUR") {
+                symbol = "€"
+            }
+            var fromSymbol = from == "USD" ? fromSymbol = "$" : fromSymbol = from + " ";
+            if (from == "EUR") {
+                fromSymbol = "€"
+            }
 
             $http.get('/currency/get?from=' + from + '&to=' + to).
                 success(function (data) {
                     console.log(data.rate);
-                    $scope.message = $filter('currency')($scope.amt * data.rate, symbol);
+                    $scope.message =$filter('currency')($scope.amt,fromSymbol)+" = "
+                        +$filter('currency')($scope.amt * data.rate, symbol);
                 });
         };
 
