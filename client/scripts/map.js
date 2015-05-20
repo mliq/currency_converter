cad7c8c627f83d59889c63043ce6a311cde32a7c
mliq <liquori@gmail.com>
2015-05-20 09:20:06 -0500

    Not sure why preset buttons don't work anymore

--- client/scripts/map.js
+++ client/scripts/map.js
@@ -1,6 +1,19 @@
 window.onload = function () {
     setupMap();
 };
+
+var mapApp = angular.module('mapApp', []);
+mapApp.controller('MapController', ['$scope', '$http', function ($scope, $http) {
+    $scope.addMarker = function (curr) {
+        $http.get('https://restcountries.eu/rest/v1/currency/eur').
+            success(function (data) {
+                console.log(data);
+            });
+    };
+    $scope.addMarker();
+}]);
+
+
 function setupMap() {
     L.mapbox.accessToken = 'pk.eyJ1IjoibWxpcSIsImEiOiJUWnMzTG13In0.o1lqB-cy9XNlBrbJ3D_ATg';
     window.map = L.mapbox.map('map', 'mliq.c23716af', {maxZoom: 2, minZoom: 2, zoomControl: false});
@@ -24,6 +37,20 @@ function setupMap() {
             }
         },
             {
+                "type": "Feature",
+                "properties": {
+                    title: "Chile",
+                    'marker-symbol': 'c'
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        -72.0703125,
+                        -36.597889133070204
+                    ]
+                }
+            },
+            {
                 type: 'Feature',
                 properties: {
                     title: 'Euro',
@@ -33,9 +60,10 @@ function setupMap() {
                 },
                 geometry: {
                     type: 'Point',
-                    coordinates: [9.49,49.152969]
+                    coordinates: [9.49, 49.152969]
                 }
-            }]
+            }
+        ]
     };
 
     myLayer.setGeoJSON(geojson);
